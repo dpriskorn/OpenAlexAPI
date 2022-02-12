697d0afbb64920e5fd0e12e40b22d04472bd1af0
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-12 18:53:06 +0100

    New test of doi namespace

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -6,9 +6,23 @@ from openalexapi import OpenAlex, Work
 
 
 class TestOpenAlex(TestCase):
-    def test_get_work(self):
+    def test_get_single_work(self):
         oa = OpenAlex()
-        work = oa.get_work("W2741809807")
-        print(work.dict())
+        work = oa.get_single_work("W2741809807")
+        # print(work.dict())
         if not isinstance(work, Work):
             self.fail()
+
+    def test_get_single_work_via_doi_namespace(self):
+        oa = OpenAlex()
+        work = oa.get_single_work("doi:10.7717/peerj.4375")
+        # print(work.dict())
+        if not isinstance(work, Work):
+            self.fail()
+
+    # def test_get_single_work_via_pmid_namespace(self):
+    #     oa = OpenAlex()
+    #     work = oa.get_single_work("pmid:29456894")
+    #     print(work.dict())
+    #     if not isinstance(work, Work):
+    #         self.fail()

04630ede027d66b61404c13f1510951f969a0f89
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-13 11:03:01 +0100

    Make oa_status and oa_url optional

--- openalexapi/openaccess.py
+++ openalexapi/openaccess.py
@@ -1,11 +1,13 @@
+from typing import Optional
+
 from purl import URL
 from pydantic import BaseModel
 
 
 class OpenAccess(BaseModel):
     is_oa: bool
-    oa_status: str
-    oa_url: str
+    oa_status: Optional[str]
+    oa_url: Optional[str]
 
     class Config:
         arbitrary_types_allowed = True

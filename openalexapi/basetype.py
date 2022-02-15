1afc4b18f24d4ae667899eb9cec06cce343e8fc5
Dennis Priskorn <68460690+dpriskorn@users.noreply.github.com>
2022-02-15 10:53:55 +0100

    Add missing property decorator

--- openalexapi/basetype.py
+++ openalexapi/basetype.py
@@ -6,6 +6,7 @@ from pydantic import BaseModel
 class OpenAlexBaseType(BaseModel):
     id: Optional[str]  # this is urls like https://openalex.org/W123
 
+    @property
     def id_without_prefix(self):
         if self.id is not None:
             return self.id.replace("https://openalex.org/", "")

9411596ebfebfec90deeb5ccd5f437ba65c35bbb
tbsgr <code@elmc.at>
2022-03-22 01:02:38 +0100

    test: add test for authors

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -2,22 +2,29 @@ from unittest import TestCase
 
 # from rich import print
 
-from openalexapi import OpenAlex, Work
+from openalexapi import OpenAlex, Work, Author
 
 
 class TestOpenAlex(TestCase):
     def test_get_single_work(self):
         oa = OpenAlex()
-        work = oa.get_single_work("W2741809807")
+        work = oa.get_single_entity("W2741809807", "works/")
         # print(work.dict())
         if not isinstance(work, Work):
             self.fail()
 
     def test_get_single_work_via_doi_namespace(self):
         oa = OpenAlex()
-        work = oa.get_single_work("doi:10.7717/peerj.4375")
+        work = oa.get_single_entity("doi:10.7717/peerj.4375", "works/")
         # print(work.dict())
         if not isinstance(work, Work):
+            self.fail()
+
+    def test_get_single_author(self):
+        oa = OpenAlex()
+        author = oa.get_single_entity("A2479313101", "authors/")
+        # print(author.dict())
+        if not isinstance(author, Author):
             self.fail()
 
     # def test_get_single_work_via_pmid_namespace(self):

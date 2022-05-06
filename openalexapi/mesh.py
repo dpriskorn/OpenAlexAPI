0263950b70e8b560dec3be467bd83b6db1b78930
noah.pacik-nelson <noah.pacik-nelson@accenture.com>
2022-05-06 17:01:11 -0400

    Made is_main_topic optional. Updated descriptor_ui

--- openalexapi/mesh.py
+++ openalexapi/mesh.py
@@ -6,8 +6,8 @@ class Mesh(BaseModel):
     """This models the mesh object at OpenAlex.
     Unfortunately it does not contain the year when the term
     was added to MESH nor if it is still a valid MESH term"""
-    descriptor_ui: constr(max_length=7, min_length=7)
-    is_main_topic: bool
+    descriptor_ui: constr(max_length=10, min_length=7)
+    is_main_topic: Optional[bool]
     descriptor_name: str
     qualifier_ui: Optional[str]
     qualifier_name: Optional[str]

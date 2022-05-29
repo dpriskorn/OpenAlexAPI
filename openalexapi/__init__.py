cd550a610be8c01e36024f193c82acf1ccce76cb
kochbj <akathriel@gmail.com>
2022-05-29 12:36:32 -0400

    Generalized get works
    -Changed works to hidden method called get entities so that it works with authors, concepts, institutions, etc....
    -Added global PAGE_LIMIT variable
    -Added  method to set_email since it wasn't clear to me initially how to do that.

--- openalexapi/__init__.py
+++ openalexapi/__init__.py
@@ -8,7 +8,18 @@ import backoff  # type: ignore
 import requests
 from pydantic import BaseModel, EmailStr
 
+from openalexapi.basetype import OpenAlexBaseType
 from openalexapi.work import Work
+from openalexapi.author import Author
+from openalexapi.work import Concept
+from openalexapi.venue import Venue
+from openalexapi.institution import Institution
+
+PAGE_LIMIT = 50 # Limit of 200 is imposed by OpenAlex API
+
+
+
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,13 +32,16 @@ class OpenAlex(BaseModel):
     """
     email: Optional[EmailStr]
     base_url = "https://api.openalex.org/"
+    
+    def set_email(self,email: Optional[EmailStr]):
+        self.email = email
 
     @backoff.on_exception(backoff.expo,
-                          (requests.exceptions.Timeout,
-                           requests.exceptions.ConnectionError),
-                          max_time=60,
-                          on_backoff=print(f"Backing off"))
-    def get_single_work(self, id: str) -> Optional[Work]:
+                      (requests.exceptions.Timeout,
+                       requests.exceptions.ConnectionError),
+                      max_time=60,
+                      on_backoff=print(f"Backing off"))
+    def __get_single_entity(self, id: str, oatype: OpenAlexBaseType):
         """This models the single work entity endpoint
 
         :parameter id can be and OpenAlex ID e.g. "W123" or a namespace ID like "doi:10.123"
@@ -39,7 +53,7 @@ class OpenAlex(BaseModel):
                   "Please be nice and supply your email as the first argument "
                   "when calling this class to get into the polite pool. This way "
                   "OpenAlex can contact you if needed.")
-        url = self.base_url + "works/" + id
+        url = self.base_url + oatype.__name__.lower() + 's/' + id
         logger.debug(f"Fetching {url}")
         headers = {
             "Accept": "application/json",
@@ -47,19 +61,60 @@ class OpenAlex(BaseModel):
         }
         response = requests.get(url, headers=headers)
         if response.status_code == 200:
-            return Work(**response.json())
+            return oatype(**response.json())
         elif response.status_code == 404:
             return None
         else:
             raise ValueError(f"Got {response.status_code} from OpenAlex")
+    
+    @backoff.on_exception(backoff.expo,
+                      (requests.exceptions.Timeout,
+                       requests.exceptions.ConnectionError),
+                      max_time=60,
+                      on_backoff=print(f"Backing off"))
+    def get_single_work(self, id: str) -> Optional[Work]:
+        return self.__get_single_entity(id, Work)
+    
+    @backoff.on_exception(backoff.expo,
+                      (requests.exceptions.Timeout,
+                       requests.exceptions.ConnectionError),
+                      max_time=60,
+                      on_backoff=print(f"Backing off"))
+    def get_single_author(self, id: str) -> Optional[Author]:
+        return self.__get_single_entity(id, Author)
+    
+    @backoff.on_exception(backoff.expo,
+                          (requests.exceptions.Timeout,
+                           requests.exceptions.ConnectionError),
+                          max_time=60,
+                          on_backoff=print(f"Backing off"))
+    def get_single_concept(self, id: str) -> Optional[Concept]:
+        return self.__get_single_entity(id, Concept)
 
+    @backoff.on_exception(backoff.expo,
+                      (requests.exceptions.Timeout,
+                       requests.exceptions.ConnectionError),
+                      max_time=60,
+                      on_backoff=print(f"Backing off"))    
+    def get_single_venue(self, id: str) -> Optional[Venue]:
+        return self.__get_single_entity(id, Venue)
+    
+    @backoff.on_exception(backoff.expo,
+                  (requests.exceptions.Timeout,
+                   requests.exceptions.ConnectionError),
+                  max_time=60,
+                  on_backoff=print(f"Backing off"))    
+    def get_single_institution(self, id: str) -> Optional[Institution]:
+        return self.__get_single_entity(id, Venue)
+        
+    
     # TODO: Adapt this to support multiple namespaces
     @backoff.on_exception(backoff.expo,
                           (requests.exceptions.Timeout,
                            requests.exceptions.ConnectionError),
                           max_time=60,
                           on_backoff=print(f"Backing off"))
-    def get_multiple_works(self, ids: List[str]) -> List[Work]:
+    def __get_multiple_entities(self, ids: List[str], oatype: OpenAlexBaseType) -> List[Work]:
         """Fetches multiple works by OpenAlex IDs. Note this does not support
         alternative namespaces.
 
@@ -77,20 +132,59 @@ class OpenAlex(BaseModel):
             "User-Agent": f"OpenAlexAPI https://github.com/dpriskorn/OpenAlexAPI mailto:{self.email}"
         }
         ids = [s.replace("https://openalex.org/", "") for s in ids]
-        works = []
-        # Limit of 50 is imposed by OpenAlex API
-        for i in range(0, len(ids), 50):
-            url_ids = '|'.join(ids[i:i+50])
-            url = self.base_url + f"works?filter=openalex_id:{url_ids}&per_page=50"
-            logger.debug(f"Fetching works {i} through {i+50}")
+        entities = []
+        for i in range(0, len(ids), PAGE_LIMIT):
+            url_ids = '|'.join(ids[i:i+PAGE_LIMIT])
+            url = self.base_url + f"{oatype.__name__.lower()}s?filter=openalex_id:{url_ids}&per_page={PAGE_LIMIT}"
+            logger.debug(f"Fetching {oatype.__name__.lower()}s {i} through {i+PAGE_LIMIT}")
             response = requests.get(url, headers=headers)
             if response.status_code == 200:
-                works += [Work(**w) for w in response.json()['results']]
+                entities += [oatype(**e) for e in response.json()['results']]
             elif response.status_code == 403:
                 raise ValueError("Got error 403. Are you using OpenAlex IDs?")
             else:
                 raise ValueError(f"Got {response.status_code} from OpenAlex")
-        return works
+        return entities
+
+    @backoff.on_exception(backoff.expo,
+                  (requests.exceptions.Timeout,
+                   requests.exceptions.ConnectionError),
+                  max_time=60,
+                  on_backoff=print(f"Backing off"))    
+    def get_multiple_works(self, ids: List[str]) -> List[Work]:
+        return self.__get_multiple_entities(ids, Work)
+
+    @backoff.on_exception(backoff.expo,
+                  (requests.exceptions.Timeout,
+                   requests.exceptions.ConnectionError),
+                  max_time=60,
+                  on_backoff=print(f"Backing off"))    
+    def get_multiple_authors(self, ids: List[str]) -> List[Author]:
+        return self.__get_multiple_entities(ids, Author)
+
+    @backoff.on_exception(backoff.expo,
+                  (requests.exceptions.Timeout,
+                   requests.exceptions.ConnectionError),
+                  max_time=60,
+                  on_backoff=print(f"Backing off"))    
+    def get_multiple_concepts(self, ids: List[str]) -> List[Author]:
+        return self.__get_multiple_entities(ids, Author)
+
+    @backoff.on_exception(backoff.expo,
+                  (requests.exceptions.Timeout,
+                   requests.exceptions.ConnectionError),
+                  max_time=60,
+                  on_backoff=print(f"Backing off"))    
+    def get_multiple_venues(self, ids: List[str]) -> List[Venue]:
+        return self.__get_multiple_entities(ids, Venue)
+
+    @backoff.on_exception(backoff.expo,
+                  (requests.exceptions.Timeout,
+                   requests.exceptions.ConnectionError),
+                  max_time=60,
+                  on_backoff=print(f"Backing off"))    
+    def get_multiple_institutions(self, ids: List[str]) -> List[Institution]:
+        return self.__get_multiple_entities(ids, Institution)
 
     @backoff.on_exception(backoff.expo,
                           (requests.exceptions.Timeout,
@@ -136,7 +230,7 @@ class OpenAlex(BaseModel):
             "Accept": "application/json",
             "User-Agent": f"OpenAlexAPI https://github.com/dpriskorn/OpenAlexAPI mailto:{self.email}"
         }
-        per_page = 200 if limit is None else min(200, limit)
+        per_page = PAGE_LIMIT if limit is None else min(PAGE_LIMIT, limit)
         works = []
         cursor = '*'
         while cursor:
